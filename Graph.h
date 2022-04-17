ed3e68feb97d35d1d7ba7d52467d3fb9a5490a5e
savelevaleksandr <dvikt3@gmail.com>
2022-04-17 19:18:48 +0300

    TravelingSalesman method added

--- Graph.h
+++ Graph.h
@@ -1,14 +1,14 @@
 #include <vector>
 #include <cstdint>
 using namespace std;
+
 class Graph {
 public:
     Graph(size_t n_vertex);
-    void AddEdge(size_t from, size_t to, size_t length);
-
-
-    vector <size_t> GetEdgesFrom(size_t from) const;
+    void AddEdge(size_t from, size_t to, int length);
+    vector<size_t> GetEdgesFrom(size_t from) const;
+    vector<size_t> TravelingSalesman();
 private:
-    vector<vector<size_t>> adj_matrix; //нумерация вершин с нуля
+    vector<vector<int>> adj_matrix; //нумерация вершин с нуля
     size_t n_vertex; //количество вершин
 };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 #include "Graph.h"
 
-int main() {
+size_t overlap(string s1, string s2);
 
+int main() {
+    int n;
+    std::cin >> n;
+    vector<string> strings(n);
+    for (int i = 0; i < n; i++) {
+        cin >> strings[i];
+    }
+    Graph graph (n);
+    for (size_t from = 0; from < n; from++) {
+        for (size_t to = 0; to < n; to++) {
+            if (from == to)
+                continue;
+            graph.AddEdge(from, to, overlap(strings[from], strings[to]));
+        }
+    }
+    for (auto i : graph.TravelingSalesman()) {
+        cout << strings[i] << " ";
+    }
+}
+
+size_t overlap(string s1, string s2) {
+    size_t overlap = 0;
+    for (int i = 1; i <= min(s1.length(), s2.length()); i++) {
+        if (s1.substr(s1.length() - i, i) == s2.substr(0, i))
+            overlap = i;
+    }
+    return overlap;
 }
